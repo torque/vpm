ef8b7fedeb9875699cd198f0d162a91bb628c83a
torque <torque@1>
2015-08-13 14:23:35 -0700

    CommonLog: allow file-local log level override.
    The problem with using a const static variable instead of a define is
    that it can't be redefined later. Thus, providing overridable
    target-specific defaults becomes messier. Perhaps I will figure out a
    better way to do this in the future.

--- src/CommonLog.h
+++ src/CommonLog.h
@@ -5,8 +5,12 @@
 // DDLogLevelInfo
 // DDLogLevelDebug
 // DDLogLevelVerbose
-#if defined(DEBUG)
-	const static DDLogLevel ddLogLevel = DDLogLevelVerbose;
+#if defined(VPM_LOGLEVELOVERRIDE)
+	const static DDLogLevel ddLogLevel = VPM_LOGLEVELOVERRIDE;
 #else
-	const static DDLogLevel ddLogLevel = DDLogLevelWarning;
+	#if defined(DEBUG)
+		const static DDLogLevel ddLogLevel = DDLogLevelVerbose;
+	#else
+		const static DDLogLevel ddLogLevel = DDLogLevelWarning;
+	#endif
 #endif
